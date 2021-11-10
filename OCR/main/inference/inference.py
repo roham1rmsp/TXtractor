cddf2d437de5881ad5fb4e76fd639e7864285600
Moeed <ppro78741@gmail.com>
2021-11-10 23:10:51 +0330

    Completed the inference part, Ready to learn new techniques

---
+++ OCR/main/inference/inference.py
@@ -0,0 +1,30 @@
+import os
+import numpy as np
+from extractor import Extract
+from text.separator import Cluster
+from text.serialization import SaveAsText
+
+
+
+class Infer:
+    def __init__(self):
+        self.path = os.path.sep.join([os.getcwd().replace(
+            "inference", "images"), "experiment_1", "sample.jpg"])
+        self.extractor = Extract(self.path)
+        self.cluster = Cluster
+        self.as_text = SaveAsText
+
+    def extract(self):
+        return self.extractor.xtract()
+
+    def begin(self):
+        coords, x_letters = self.extract()
+        self.cluster = self.cluster(np.array(coords), np.array(x_letters))
+        words = self.cluster.sepr()
+        self.as_text = self.as_text(words)
+        self.as_text.save()
+
+
+if __name__ == "__main__":
+    infer = Infer()
+    infer.begin()
---
+++ OCR/main/inference/utils.py
@@ -0,0 +1,42 @@
+import cv2
+import numpy as np 
+from spellchecker import SpellChecker
+
+
+class Process:
+    __kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (5, 5))
+    __clahe = cv2.createCLAHE(clipLimit=1.5, tileGridSize=(8, 8))
+
+    def _to_gray(self, image: np.ndarray) -> np.ndarray:
+        return cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    def _denose(self, image: np.ndarray) -> np.ndarray:
+        return cv2.fastNlMeansDenoising(image, None, 15, 7, 21)
+
+    def _clahe(self, image: np.ndarray) -> np.ndarray:
+        return self.__clahe.apply(image)
+
+    def _blur(self, image: np.ndarray) -> np.ndarray:
+        return cv2.GaussianBlur(image, (5, 5), 0)
+
+    def _find_edges(self, image: np.ndarray) -> np.ndarray:
+        return cv2.Canny(image, 30, 150)
+
+    def _close(self, image: np.ndarray) -> np.ndarray:
+        return cv2.dilate(cv2.morphologyEx(image,
+                 cv2.MORPH_CLOSE, self.__kernel), self.__kernel, iterations=1)
+
+class CleanUp:
+    def __init__(self):
+        self.words = []
+        self.spell = SpellChecker()
+
+    def clean(self, words):
+        for word in words:
+            misspelled = self.spell.unknown([word])
+            if len(misspelled) == 0:
+                self.words.append(word)
+                continue
+            for word in misspelled:
+                self.words.append(self.spell.correction(word))
+        return self.words
